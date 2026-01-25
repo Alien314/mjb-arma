f22a750f41979b10aa9b3c2423a894a2a35d5ab1
Alien314 <stealth131l30@yahoo.com>
2026-01-25 09:43:53 -0600

    MCC sights tweaks

---
+++ addons/configtweaks/MCC_sights_tweaks/config.cpp
@@ -0,0 +1,22 @@
+class CfgPatches {
+  class mjb_mcc_sights_tweaks {
+    ammo[] = {};
+    magazines[] = {};
+    units[] = {};
+    weapons[] = {};
+    requiredVersion = 0.1;
+    author = "Momo";
+    name = "MCC sights tweaks";
+	requiredAddons[]=
+        {
+            "MCC_RD",//weapons[] = {"MCC_MicroT2","MCC_MicroT2_Down","MCC_MicroT2_Up","MCC_EXPS3_BLK","MCC_EXPS3_BLK_Down","MCC_EXPS3_BLK_Up","MCC_EXPS3_FDE","MCC_EXPS3_FDE_Down","MCC_EXPS3_FDE_Up","MCC_Romeo9t_blk","MCC_Romeo9t_fde","MCC_Romeo9t_BLK_Down","MCC_Romeo9T_BLK_Up","MCC_Romeo9T_FDE_Down","MCC_Romeo9T_FDE_Up"};
+            "MCC_LPVO",//weapons[] = {"MCC_Vortex_Elanor_Acro_BLK","MCC_Vortex_Elanor_Acro_FDE","MCC_Vortex_Elanor_Acro_TAN","MCC_Mark5_10_BLK","MCC_Mark5_10_FDE","MCC_Trijicon_VCOG_18","MCC_ZCO_10_BLK_DMR"};
+            "MCC_ERD"//weapons[] = {"MCC_GBRS_T2_FDE","MCC_GBRS_T2_Mag_Down_FDE","MCC_GBRS_T2_Mag_Up_FDE","MCC_GBRS_T2_Mag_Up_BLK","MCC_GBRS_EXPS3_FDE","MCC_GBRS_EXPS3_Mag_Down_FDE","MCC_GBRS_EXPS3_Mag_Up_FDE","MCC_GBRS_EXPS3_Mag_Up_BLK","MCC_MicroT2_UnityX_BLK","MCC_MicroT2_UnityX_FDE","MCC_MicroT2_UnityX_BLK_Down","MCC_MicroT2_UnityX_BLK_Up","MCC_MicroT2_UnityX_FDE_Down","MCC_MicroT2_UnityX_FDE_Up","MCC_EXPS3_UnityX_BLK","MCC_EXPS3_UnityX_BLK_Down","MCC_EXPS3_UnityX_BLK_Up","MCC_EXPS3_UnityX_FDE","MCC_EXPS3_UnityX_FDE_Down","MCC_EXPS3_UnityX_FDE_Up"};
+        };
+    skipWhenMissingDependencies = 1;
+  };
+};
+
+class CfgWeapons {
+
+};
