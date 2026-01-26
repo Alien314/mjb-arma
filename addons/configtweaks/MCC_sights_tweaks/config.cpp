bdd2d34d71f9e02a27ac035fd1a0bd81f5a833ba
Alien314 <93859311+Alien314@users.noreply.github.com>
2026-01-25 20:19:32 -0600

    MCC config is inconsistent

--- addons/configtweaks/MCC_sights_tweaks/config.cpp
+++ addons/configtweaks/MCC_sights_tweaks/config.cpp
@@ -22,8 +22,8 @@ class CfgWeapons {
 	class ItemCore;
 	class InventoryOpticsItem_Base_F;
 	
-	// Macro: Sights Down/Red Dots
-	#define MCC_DOT(var) class var : ItemCore;
+	// Macro: Sights Red Dots
+	#define MCC_DOT(var) class var : ItemCore { \
 		class ItemInfo : InventoryOpticsItem_Base_F { \
 	    	class OpticsModes { \
 				class Aco { \
@@ -31,11 +31,23 @@ class CfgWeapons {
 	        		discreteDistanceInitIndex = 0; \
 				}; \
 			}; \
+		}; \
+	};
+	
+	// Macro: Sights optics Down. (base,var reversed)
+	#define MCC_DWN(base,var) class var : base { \
+			class ItemInfo : InventoryOpticsItem_Base_F { \
+	      		class OpticsModes { \
+					class Aco { \
+						discreteDistance[] = {100}; \
+						discreteDistanceInitIndex = 0; \
+					}; \
+	    		}; \
+	  		}; \
 		}
 	
 	// Macro: Sights optics Up.
-	#define MCC_UP(var,base) class base##;
-		class var : base { \
+	#define MCC_UP(var,base) class var : base { \
 			class ItemInfo : InventoryOpticsItem_Base_F { \
 	      		class OpticsModes { \
 	        		class Magnified { \
@@ -48,23 +60,35 @@ class CfgWeapons {
 	
 	// 1x / red dots
 	MCC_DOT(MCC_EXPS3_BLK);
-	MCC_DOT(MCC_EXPS3_BLK_Down);
 	MCC_DOT(MCC_EXPS3_FDE);
-	MCC_DOT(MCC_EXPS3_FDE_Down);
-	MCC_DOT(MCC_GBRS_EXPS3_BLK);
 	MCC_DOT(MCC_GBRS_EXPS3_FDE);
-	MCC_DOT(MCC_GBRS_EXPS3_Mag_Down_BLK);
-	MCC_DOT(MCC_GBRS_EXPS3_Mag_Down_FDE);
-	MCC_DOT(MCC_EXPS3_UnityX_BLK_Down);
-	MCC_DOT(MCC_EXPS3_UnityX_FDE_Down);
 	MCC_DOT(MCC_Romeo9t_blk);
-	MCC_DOT(MCC_Romeo9t_BLK_Down);
+	MCC_DOT(MCC_Romeo9t_fde);
 	MCC_DOT(MCC_MicroT2);
-	MCC_DOT(MCC_MicroT2_UnityX_BLK_Down);
-	MCC_DOT(MCC_MicroT2_UnityX_FDE_Down);
-	MCC_DOT(MCC_GBRS_T2_BLK);
-	MCC_DOT(MCC_GBRS_T2_Mag_Down_BLK);
-	MCC_DOT(MCC_GBRS_T2_Mag_Down_FDE);
+	MCC_DOT(MCC_MicroT2_UnityX_BLK);
+	MCC_DOT(MCC_GBRS_T2_FDE);
+	//MCC_DWN(MCC_GBRS_T2_FDE,MCC_GBRS_T2_BLK);
+	//MCC_DWN(MCC_MicroT2_UnityX_BLK,MCC_MicroT2_UnityX_FDE);
+	//MCC_DWN(MCC_GBRS_EXPS3_FDE,MCC_GBRS_EXPS3_BLK);
+
+	// 1x / optics down
+	MCC_DWN(MCC_EXPS3_BLK,MCC_EXPS3_BLK_Down);
+	MCC_DWN(MCC_EXPS3_FDE,MCC_EXPS3_FDE_Down);
+	MCC_DWN(MCC_GBRS_EXPS3_FDE,MCC_GBRS_EXPS3_Mag_Down_FDE);
+	//MCC_DWN(MCC_GBRS_EXPS3_Mag_Down_FDE,MCC_GBRS_EXPS3_Mag_Down_BLK);
+	//MCC_DWN(MCC_EXPS3_BLK_Down,MCC_EXPS3_UnityX_BLK_Down);
+	//MCC_DWN(MCC_EXPS3_FDE_Down,MCC_EXPS3_UnityX_FDE_Down);
+	MCC_DWN(MCC_Romeo9t_blk,MCC_Romeo9t_BLK_Down);
+	MCC_DWN(MCC_Romeo9t_fde,MCC_Romeo9T_FDE_Down);
+	MCC_DWN(MCC_MicroT2,MCC_MicroT2_Down);
+	MCC_DWN(MCC_MicroT2,MCC_MicroT2_UnityX_BLK_Down);
+	MCC_DWN(MCC_MicroT2,MCC_MicroT2_UnityX_FDE_Down);
+	MCC_DWN(MCC_GBRS_EXPS3_FDE,MCC_GBRS_T2_Mag_Down_FDE);
+	//MCC_DWN(MCC_GBRS_T2_Mag_Down_FDE,MCC_GBRS_T2_Mag_Down_BLK);
+	class MCC_GBRS_EXPS3_Mag_Down_BLK;
+	class MCC_EXPS3_UnityX_BLK_Down;
+	class MCC_EXPS3_UnityX_FDE_Down;
+	class MCC_GBRS_T2_Mag_Down_BLK;
 	
 	// 3x / optics up
 	MCC_UP(MCC_EXPS3_BLK_Up,MCC_EXPS3_BLK_Down);
@@ -79,5 +103,5 @@ class CfgWeapons {
 	MCC_UP(MCC_MicroT2_UnityX_BLK_Up,MCC_MicroT2_UnityX_BLK_Down);
 	MCC_UP(MCC_MicroT2_UnityX_FDE_Up,MCC_MicroT2_UnityX_FDE_Down);
 	MCC_UP(MCC_GBRS_T2_Mag_Up_BLK,MCC_GBRS_T2_Mag_Down_BLK);
-	MCC_UP(MCC_GBRS_T2_Mag_Up_FDE,MCC_EXPS3_FDE_Down);
+	MCC_UP(MCC_GBRS_T2_Mag_Up_FDE,MCC_GBRS_T2_Mag_Down_FDE);
 };
