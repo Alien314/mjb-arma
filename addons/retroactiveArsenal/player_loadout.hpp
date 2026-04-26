562585832c12400c521f29b3492d3921b078539e
Alien314 <stealth131l30@yahoo.com>
2026-04-26 16:52:42 -0500

    return AR backpack belts

--- addons/retroactiveArsenal/player_loadout.hpp
+++ addons/retroactiveArsenal/player_loadout.hpp
@@ -277,8 +277,8 @@ class ar : basetrooper
 		#else
 			LIST_3("FirstAidKit"),
 		#endif
-		LIST_4("CUP_100Rnd_TE4_Yellow_Tracer_556x45_M249")
-		//LIST_3("greenmag_beltlinked_556x45_basic_200")
+		LIST_4("CUP_100Rnd_TE4_Yellow_Tracer_556x45_M249"),
+		LIST_3("greenmag_beltlinked_556x45_basic_200")
 	};
 };
 
