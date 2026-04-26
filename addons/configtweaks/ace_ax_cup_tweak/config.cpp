ef5fe06bc7a63857ac93ccf016806414cf4aa5c7
Alien314 <stealth131l30@yahoo.com>
2026-04-26 16:54:41 -0500

    crye tropentarn fix

---
+++ addons/configtweaks/ace_ax_cup_tweak/config.cpp
@@ -0,0 +1,37 @@
+class CfgPatches {
+  class mjb_ace_ax_cup_tweak {
+		ammo[] = {};
+		magazines[] = {};
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		author = "Alien314";
+		name = "ACE AX Cup tweak";
+		requiredAddons[]=
+		{
+			"aceax_compat_cup"
+		};
+		skipWhenMissingDependencies = 1;
+	};
+};
+
+class XtdGearInfos {
+	class CfgWeapons {
+		class CUP_U_B_GER_Crye {
+			camo = "TRPTRN";
+		};
+		class CUP_U_B_GER_Crye2 {
+			camo = "TRPTRN";
+		};
+	};
+};
+
+class XtdGearModels {
+	class CfgWeapons {
+		class cup_crye {
+			class camo {
+				values[] += {"TRPTRN"};
+			};
+		};
+	};
+};
