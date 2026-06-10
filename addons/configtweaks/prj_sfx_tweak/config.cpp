6b1c949a3d74653646662de0410d323c3357ed46
Alien314 <stealth131l30@yahoo.com>
2026-06-10 10:52:54 -0500

    softer soft

--- addons/configtweaks/prj_sfx_tweak/config.cpp
+++ addons/configtweaks/prj_sfx_tweak/config.cpp
@@ -30,7 +30,7 @@ class CfgAmmo
 		impactGlass[] = {"soundImpactHard1",0.3,"soundImpactHard2",0.3,"soundImpactHard3",0.3,"soundImpactHard4",0.2,"soundImpactHard5",0.2,"soundImpactHard6",0.2,"soundImpactHard7",0.2};
 		impactGlassArmored[] = {"soundImpactHard1",0.3,"soundImpactHard2",0.3,"soundImpactHard3",0.3,"soundImpactHard4",0.2,"soundImpactHard5",0.2,"soundImpactHard6",0.2,"soundImpactHard7",0.2};
 		impactGroundHard[] = {"soundImpactHard1",0.3,"soundImpactHard2",0.3,"soundImpactHard3",0.3,"soundImpactHard4",0.2,"soundImpactHard5",0.2,"soundImpactHard6",0.2,"soundImpactHard7",0.2};
-		impactGroundSoft[] = {"soundImpactSoft1",0.3,"soundImpactSoft2",0.3,"soundImpactSoft3",0.3,"soundImpactSoft4",0.2,"soundImpactSoft5",0.2,"soundImpactSoft6",0.2,"soundImpactSoft7",0.2};
+		impactGroundSoft[] = {"soundImpactSoft1",0.2,"soundImpactSoft2",0.2,"soundImpactSoft3",0.2,"soundImpactSoft4",0.1,"soundImpactSoft5",0.1,"soundImpactSoft6",0.1,"soundImpactSoft7",0.1};
 		impactIron[] = {"soundImpactIron1",0.3,"soundImpactIron2",0.3,"soundImpactIron3",0.3,"soundImpactIron4",0.3,"soundImpactIron5",0.3};
 		impactMan[] = {"soundImpactMan1",0.35,"soundImpactMan2",0.35,"soundImpactMan3",0.35,"soundImpactMan4",0.35};
 		impactMetal[] = {"soundImpactIron1",0.3,"soundImpactIron2",0.3,"soundImpactIron3",0.3,"soundImpactIron4",0.3,"soundImpactIron5",0.3};
