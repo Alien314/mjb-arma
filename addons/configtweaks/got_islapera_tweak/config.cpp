3c155a9a6fbb9841f373419e032c95377988b55c
Alien314 <stealth131l30@yahoo.com>
2026-06-19 03:26:00 -0500

    awawawawa

---
+++ addons/configtweaks/got_islapera_tweak/config.cpp
@@ -0,0 +1,27 @@
+class CfgPatches {
+  class mjb_GOT_Map_IslaPera_tweak {
+    ammo[] = {};
+    magazines[] = {};
+    units[] = {};
+    weapons[] = {};
+    requiredVersion = 0.1;
+    author = "Alien314";
+    name = "Isla Pera tweak";
+	requiredAddons[]=
+    {
+        "GOT_Map_IslaPera"
+	};
+    skipWhenMissingDependencies = 1;
+  };
+};
+
+class CfgWorlds {
+	class CAWorld;
+	class IslaPera : CAWorld {
+		class Names {
+			class LagunaClara {
+				name = "Laguna Awawawawawawa";
+			};
+		};
+	};
+};
