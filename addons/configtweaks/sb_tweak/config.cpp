b8da7ce0911506536088fbbdfc5bea491ad37339
Alien314 <stealth131l30@yahoo.com>
2026-08-05 11:37:31 -0500

    mini yabhon noise

--- addons/configtweaks/sb_tweak/config.cpp
+++ addons/configtweaks/sb_tweak/config.cpp
@@ -31,6 +31,107 @@ class CfgFunctions
 };
 
 class CfgVehicles {
+	class B_UAV_02_dynamicLoadout_F;
+	class SwitchBlade_Base : B_UAV_02_dynamicLoadout_F {
+		class Sounds {
+			class EngineHighIn;
+			class EngineHighOut;
+			class EngineLowIn;
+			class EngineLowOut;
+			class ForsageIn;
+			class ForsageOut;
+		};
+	};
+	class B_SwitchBlade_300 : SwitchBlade_Base {
+		soundEngineOffExt[] = {"z\jsrs2025\addons\sounds_air\sounds\drones\fpv_drones_small\stop_ext.ogg",2,1,50};
+		soundEngineOffInt[] = {"z\jsrs2025\addons\sounds_air\sounds\drones\fpv_drones_small\stop_int.ogg",2,1};
+		soundEngineOnExt[] = {"z\jsrs2025\addons\sounds_air\sounds\drones\fpv_drones_small\start_ext.ogg",2,1,50};
+		soundEngineOnInt[] = {"z\jsrs2025\addons\sounds_air\sounds\drones\fpv_drones_small\start_int.ogg",2,1};
+		//soundEngineOffExt[] = {"A3\Sounds_F_Orange\Vehicles\Air\UAV_01\Uav_01_Eng_Off_Ext",1.75849,1,50};
+		//soundEngineOffInt[] = {"A3\Sounds_F_Orange\Vehicles\Air\UAV_01\Uav_01_Eng_Off_Ext",1.75849,1,10};
+		//soundEngineOnExt[] = {"A3\Sounds_F_Orange\Vehicles\Air\Uav_01\Uav_01_Eng_On_Ext",1.75849,1,50};
+		//soundEngineOnInt[] = {"A3\Sounds_F_Orange\Vehicles\Air\Uav_01\Uav_01_Eng_On_Int",1.75849,1,10};
+		//soundFlapsDown[] = {"",0.630957,1,100};
+		//soundFlapsUp[] = {"",0.630957,1,100};
+		//soundGearDown[] = {"",1,1,120};
+		//soundGearUp[] = {"",1,1,120};
+
+		class Sounds : Sounds {
+			soundsetsext[] = {"jsrs_2025_drones_fpv_small_engineext_soundset","jsrs_2025_drones_fpv_small_rotorext_soundset","jsrs_2025_drones_fpv_small_evr_soundset","jsrs_2025_drones_fpv_small_rotordistance_soundset"};
+			soundsetsint[] = {"jsrs_2025_drones_fpv_small_engineint_soundset","jsrs_2025_drones_fpv_small_rotorint_soundset"};
+			class EngineHighIn : EngineHighIn {
+				frequency = "(rpm factor[0.5, 1.0])";
+				sound[] = {"A3\Sounds_F\air\UAV_02\UAV_02_high_int",1,1};
+				volume = "(1-camPos)*(rpm factor[0.2, 1.0])";
+			};
+			class EngineHighOut : EngineHighOut {
+				frequency = "(rpm factor[0.5, 1.0])";
+				sound[] = {"A3\Sounds_F\air\UAV_02\UAV_02_high_ext",1,1,250};
+				volume = "camPos*(rpm factor[0.2, 1.0])";
+			};
+			class EngineLowIn : EngineLowIn {
+				frequency = "1.0 min (rpm + 0.5)";
+				sound[] = {"A3\Sounds_F\air\UAV_02\UAV_02_low_int",1,1};
+				volume = "(1-camPos)*(rpm factor[0.95, 0])*(rpm factor[0, 0.95])";
+			};
+			class EngineLowOut : EngineLowOut {
+				frequency = "1.0 min (rpm + 0.5)";
+				sound[] = {"A3\Sounds_F\air\UAV_02\UAV_02_low_ext",0.707946,1,175};
+				volume = "camPos*(rpm factor[0.95, 0])*(rpm factor[0, 0.95])";
+			};
+		};
+		class soundsext {
+			class Sounds {
+				soundsetsext[] = {"jsrs_2025_drones_fpv_small_engineext_soundset","jsrs_2025_drones_fpv_small_rotorext_soundset","jsrs_2025_drones_fpv_small_evr_soundset","jsrs_2025_drones_fpv_small_rotordistance_soundset"};
+				soundsetsint[] = {"jsrs_2025_drones_fpv_small_engineint_soundset","jsrs_2025_drones_fpv_small_rotorint_soundset"};
+			};
+		};
+	};
+	class B_SwitchBlade_600 : SwitchBlade_Base {
+		soundEngineOffExt[] = {"z\jsrs2025\addons\sounds_air\sounds\drones\fpv_drones_small\stop_ext.ogg",2,1,100};
+		soundEngineOffInt[] = {"z\jsrs2025\addons\sounds_air\sounds\drones\fpv_drones_small\stop_int.ogg",2,1};
+		soundEngineOnExt[] = {"z\jsrs2025\addons\sounds_air\sounds\drones\fpv_drones_small\start_ext.ogg",2,1,100};
+		soundEngineOnInt[] = {"z\jsrs2025\addons\sounds_air\sounds\drones\fpv_drones_small\start_int.ogg",2,1};
+		//soundEngineOffExt[] = {"A3\Sounds_F_Orange\Vehicles\Air\UAV_01\Uav_01_Eng_Off_Ext",1.75849,1,50};
+		//soundEngineOffInt[] = {"A3\Sounds_F_Orange\Vehicles\Air\UAV_01\Uav_01_Eng_Off_Ext",1.75849,1,10};
+		//soundEngineOnExt[] = {"A3\Sounds_F_Orange\Vehicles\Air\Uav_01\Uav_01_Eng_On_Ext",1.75849,1,50};
+		//soundEngineOnInt[] = {"A3\Sounds_F_Orange\Vehicles\Air\Uav_01\Uav_01_Eng_On_Int",1.75849,1,10};
+		//soundFlapsDown[] = {"",0.630957,1,100};
+		//soundFlapsUp[] = {"",0.630957,1,100};
+		//soundGearDown[] = {"",1,1,120};
+		//soundGearUp[] = {"",1,1,120};
+
+		class Sounds : Sounds {
+			soundsetsext[] = {"jsrs_2025_drones_fpv_big_engineext_soundset","jsrs_2025_drones_fpv_big_rotorext_soundset","jsrs_2025_drones_fpv_big_evr_soundset","jsrs_2025_drones_fpv_big_rotordistance_soundset"};
+			soundsetsint[] = {"jsrs_2025_drones_fpv_big_engineint_soundset","jsrs_2025_drones_fpv_big_rotorint_soundset"};
+			class EngineHighIn : EngineHighIn {
+				frequency = "(rpm factor[0.5, 1.0])";
+				sound[] = {"A3\Sounds_F\air\UAV_02\UAV_02_high_int",1,1};
+				volume = "(1-camPos)*(rpm factor[0.2, 1.0])";
+			};
+			class EngineHighOut : EngineHighOut {
+				frequency = "(rpm factor[0.5, 1.0])";
+				sound[] = {"A3\Sounds_F\air\UAV_02\UAV_02_high_ext",1,1,300};
+				volume = "camPos*(rpm factor[0.2, 1.0])";
+			};
+			class EngineLowIn : EngineLowIn {
+				frequency = "1.0 min (rpm + 0.5)";
+				sound[] = {"A3\Sounds_F\air\UAV_02\UAV_02_low_int",1,1};
+				volume = "(1-camPos)*(rpm factor[0.95, 0])*(rpm factor[0, 0.95])";
+			};
+			class EngineLowOut : EngineLowOut {
+				frequency = "1.0 min (rpm + 0.5)";
+				sound[] = {"A3\Sounds_F\air\UAV_02\UAV_02_low_ext",0.707946,1,225};
+				volume = "camPos*(rpm factor[0.95, 0])*(rpm factor[0, 0.95])";
+			};
+		};
+		class soundsext {
+			class Sounds {
+				soundsetsext[] = {"jsrs_2025_drones_fpv_big_engineext_soundset","jsrs_2025_drones_fpv_big_rotorext_soundset","jsrs_2025_drones_fpv_big_evr_soundset","jsrs_2025_drones_fpv_big_rotordistance_soundset"};
+				soundsetsint[] = {"jsrs_2025_drones_fpv_big_engineint_soundset","jsrs_2025_drones_fpv_big_rotorint_soundset"};
+			};
+		};
+	};
 	class I_SwitchBlade_300;
 	class I_SwitchBlade_600;
 	class B_SwitchBlade_300_LaunchTube_Desert;
